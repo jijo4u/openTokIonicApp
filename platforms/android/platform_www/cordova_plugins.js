d184d03f5bee32b7cc29651083ca6c9e14a096a8
jijo alexander <mail.jijoalexander@gmail.com>
2016-12-23 11:29:46 +0530

    local commit

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -12,12 +12,23 @@ module.exports = [
         "id": "cordova-plugin-x-toast.tests",
         "file": "plugins/cordova-plugin-x-toast/test/tests.js",
         "pluginId": "cordova-plugin-x-toast"
+    },
+    {
+        "id": "cordova-plugin-opentokjs.OpenTokClient",
+        "file": "plugins/cordova-plugin-opentokjs/src/android/opentok.js",
+        "pluginId": "cordova-plugin-opentokjs",
+        "clobbers": [
+            "OT"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-x-toast": "2.5.2"
+    "cordova-plugin-x-toast": "2.5.2",
+    "cordova-plugin-crosswalk-webview": "2.2.0",
+    "cordova-plugin-iosrtc": "3.2.2",
+    "cordova-plugin-opentokjs": "0.1.2"
 };
 // BOTTOM OF METADATA
 });
