450eda7d4ce0d7cb8be014437ad3d4f2bdc437df
jijo alexander <mail.jijoalexander@gmail.com>
2016-12-27 01:40:52 +0530

    local commit by 26-12-2016

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -121,34 +121,134 @@ angular.module('starter.controllers', [])
     ionicMaterialInk.displayEffect();
 })
 
-.controller('TokboxCtrl',['$scope', '$stateParams', '$timeout', 'ionicMaterialMotion', 'ionicMaterialInk', '$cordovaToast','OTSession', 'apiKey', 'sessionId', 'token', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk, $cordovaToast, OTSession, apiKey, sessionId, token) {
+.controller('TokboxCtrl', ['$scope', '$stateParams', '$timeout', 'ionicMaterialMotion', 'ionicMaterialInk', '$cordovaToast', 'OTSession', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk, $cordovaToast, OTSession) {
     // Set Header
     $scope.$parent.showHeader();
     $scope.$parent.clearFabs();
     $scope.isExpanded = false;
     $scope.$parent.setExpanded(false);
     $scope.$parent.setHeaderFab(false);
+    var session;
+    var connectionCount = 0;
+    var apiKey = '45739362';
+    var sessionId = '1_MX40NTczOTM2Mn5-MTQ4Mjc3MzQ3NDg0Nn5yNjM5enNUMmFhbzMyYlo2L1o2WUJLdTV-UH4';
+    var token = 'T1==cGFydG5lcl9pZD00NTczOTM2MiZzaWc9ZWVmNmVmOTg1MGZiZjVhZjJhYmFjMzEyY2IzMjM3YmFlNjY1ODdhNjpzZXNzaW9uX2lkPTFfTVg0ME5UY3pPVE0yTW41LU1UUTRNamMzTXpRM05EZzBObjV5TmpNNWVuTlVNbUZoYnpNeVlsbzJMMW8yV1VKTGRUVi1VSDQmY3JlYXRlX3RpbWU9MTQ4Mjc3MzQ3NSZub25jZT0wLjgyODgyOTc4Njk3MzA3NDEmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ4Mjg1OTg3NSZjb25uZWN0aW9uX2RhdGE9dXNlcm5hbWUlM0Rib2I=';
 
- 
-     OTSession.init(apiKey, sessionId, token);
-                $scope.streams = OTSession.streams;
+    //  OTSession.init(apiKey, sessionId, token);
+    //             $scope.streams = OTSession.streams;
+    // console.log($scope.streams);
+    tost('started');
+    session = OT.initSession(apiKey, sessionId);
 
-    // Set Ink
+    function connect() {
+        // Replace apiKey and sessionId with your own values:
+        session = OT.initSession(apiKey, sessionId);
+        session.on({
+            connectionCreated: function(event) {
+                connectionCount++;
+                console.log(connectionCount + ' connections.');
+            },
+            connectionDestroyed: function(event) {
+                connectionCount--;
+                console.log(connectionCount + ' connections.');
+                tost(connectionCount + ' connections.');
+
+            },
+            sessionDisconnected: function sessionDisconnectHandler(event) {
+                // The event is defined by the SessionDisconnectEvent class
+                console.log('Disconnected from the session.');
+                // document.getElementById('disconnectBtn').style.display = 'none';
+                if (event.reason == 'networkDisconnected') {
+                    alert('Your network connection terminated.')
+                }
+            }
+        });
+        // Replace token with your own value:
+        session.connect(token, function(error) {
+            if (error) {
+                console.log('Unable to connect: ', error.message);
+                tost('Unable to connect: '+error.message);
+            } else {
+                // document.getElementById('disconnectBtn').style.display = 'block';
+                console.log('Connected to the session.');
+                tost('Connected to the session.');
+                connectionCount = 1;
+            }
+        });
+    }
+
+    $scope.disconnect = function() {
+        session.disconnect();
+    }
+
+    connect();
+
+    $scope.publish = function() {
+        var session;
+        var publisher;
+        console.log('here');
+        // Replace with the replacement element ID:
+        publisher = OT.initPublisher('publisherContainer');
+        publisher.on({
+            streamCreated: function(event) {
+                console.log("Publisher started streaming.");
+            },
+            streamDestroyed: function(event) {
+                console.log("Publisher stopped streaming. Reason: " + event.reason);
+            }
+        });
+
+        // Replace apiKey and sessionID with your own values:
+        session = OT.initSession(apiKey, sessionId);
+        // Replace token with your own value:  
+        if (session.capabilities.publish == 1) {
+            session.publish(publisher);
+        } else {
+            console.log("You cannot publish an audio-video stream.");
+        }
+    }
+
+
+
+    $scope.Subscriber = function() {
+            session = OT.initSession(apiKey, sessionId);
+
+            session.connect(token, function(error) {
+
+                if (error) {
+                    // failed to connect
+                    console.log(error);
+                } else {
+                    session.on('streamCreated', function(event) {
+                        var subscriberProperties = {
+                            insertMode: 'append'
+                        };
+                        console.log(event);
+                        var subscriber = session.subscribe(event.stream,
+                            'subscriberContainer',
+                            subscriberProperties,
+                            function(error) {
+                                if (error) {
+                                    console.log(error);
+                                } else {
+                                    console.log('Subscriber added.');
+                                }
+                            });
+                    });
+                }
+            });
+        }
+        // Set Ink
     ionicMaterialInk.displayEffect();
 
     function tost(msg) {
         $cordovaToast
-            .show(msg, 'short', 'center')
+            .show(msg, 'long', 'center')
             .then(function(success) {
                 // success
             }, function(error) {
                 // error
             });
     }
-     
-}]).value({
-                apiKey: '45739362',
-                sessionId: '2_MX40NTczOTM2Mn5-MTQ4MjUxNTczMjMyNH5SQjZMYUJwaEtpZ2hIT3l6ZkNBN2hQaUd-UH4',
-                token: 'T1==cGFydG5lcl9pZD00NTczOTM2MiZzaWc9ZGNjOTliNDQwYmI3Njk0ZTI2MTIzYzdlZTgzYTE4YzdiNTExZGU1ZjpzZXNzaW9uX2lkPTJfTVg0ME5UY3pPVE0yTW41LU1UUTRNalV4TlRjek1qTXlOSDVTUWpaTVlVSndhRXRwWjJoSVQzbDZaa05CTjJoUWFVZC1VSDQmY3JlYXRlX3RpbWU9MTQ4MjUxNTczMiZub25jZT0wLjA4ODA2OTIzMTAxNjU2MTM5JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE0ODI2MDIxMzImY29ubmVjdGlvbl9kYXRhPXVzZXJuYW1lJTNEYm9i'
-            })
-;
+
+}]);
